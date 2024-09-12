8ad25fe9358839562675b8c22bf38cb3b131334d
BrunoLima-085 <fbrunosl@gmail.com>
2024-09-12 11:19:10 -0300

    Add files via upload
    Teste Aula01 lógica

---
+++ Prova01.py
@@ -0,0 +1,5 @@
+base = float (input ("\nDigite o valor da base do retângulo: "))
+altura = float (input ("\nDigite o valor da altura do retângulo: "))
+area = base * altura
+
+print (f"\nO valor da área do retângulo é {area}")
